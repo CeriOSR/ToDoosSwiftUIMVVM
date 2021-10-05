6a94c05e2f2276005bb68ab51b7d7c7b28d04986
Rey Cerio <rey.cerio@appetiser.com.au>
2021-10-05 15:18:27 -0700

    Initial Commit

---
+++ TaskManagerCloneSwiftUI/TaskManagerCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerCloneSwiftUIApp.swift
+//  TaskManagerCloneSwiftUI
+//
+//  Created by Rey Cerio on 2021-10-05.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
